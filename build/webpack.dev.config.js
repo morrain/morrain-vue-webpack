f33bfb319984f0ab74ccfb6e6d4f7bd59c1fffad
morarin <626100676@qq.com>
2019-05-17 20:45:43 +0800

    add webpack-dev-server

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -3,9 +3,13 @@
 const merge = require('webpack-merge');
 const webpackBaseConfig = require('./webpack.base.config');
 const webpack = require('webpack');
-
+const path = require('path');
 
 const webpackConfig = merge(webpackBaseConfig, {
+    devServer: {
+        contentBase: path.resolve(__dirname, '../dist'),
+        port: 9000
+    },
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('prod')
