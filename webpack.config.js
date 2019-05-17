84627b32ab3588bee661ebfab9806d4493e952df
morarin <626100676@qq.com>
2019-05-17 10:56:57 +0800

    use two plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -25,5 +27,11 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management by HtmlWebpckPlugin'
+        })
+    ]
 };
