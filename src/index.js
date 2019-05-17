aa07efbeba3ef28a5ed25a234f6c43b977bef5df
morarin <626100676@qq.com>
2019-05-17 10:36:48 +0800

    add entry demo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './style.css';
 //导入图片
 import Icon from './logo.png';
 
+import printMe from './print';
+
 function component() {
     var element = document.createElement('div');
 
@@ -14,6 +16,13 @@ function component() {
     var myIcon = new Image();
     myIcon.src = Icon;
     element.appendChild(myIcon);
+
+    //添加打印demo
+
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Click Me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
     return element;
 }
