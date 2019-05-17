883f7ff2c6467e6c68212601906c252852ee0e54
morarin <626100676@qq.com>
2019-05-17 10:18:29 +0800

    add file-loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import _ from 'lodash';
 import './style.css';
 
+//导入图片
+import Icon from './logo.png';
+
 function component() {
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    //将图片添加到我们现有的元素
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
 
     return element;
 }
