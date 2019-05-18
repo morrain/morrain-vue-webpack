5cceac7438deca3645beb667f38e7d2f84385a04
morarin <626100676@qq.com>
2019-05-18 17:02:12 +0800

    add test demo

--- src/testBabel.js
+++ src/testBabel.js
@@ -7,3 +7,12 @@ setTimeout(() => {
 
 var array = [1, 2, 3, 4, 5, 6];
 array.includes(item => item > 2);
+
+let tpx = new Promise((resolve, reject) => {
+    console.log('new Promise done');
+});
+
+Object.assign({}, {
+    a: 1,
+    b: 2
+});
