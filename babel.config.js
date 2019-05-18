c877330d13508f16ccbadf2e07725201ef67e770
morarin <626100676@qq.com>
2019-05-18 16:50:44 +0800

    use babelpolyfill  to process es6 api

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,8 @@ module.exports = function (api) {
         [
             "@babel/env",
             {
-                debug: true
+                debug: true,
+                useBuiltIns: 'usage'
             }
         ]
     ];
