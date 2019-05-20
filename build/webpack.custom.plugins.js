9222a2ebf01ddf93dd668dbf3357092aa1b0b06b
morarin <626100676@qq.com>
2019-05-20 14:45:19 +0800

    add async event plugin

--- build/webpack.custom.plugins.js
+++ build/webpack.custom.plugins.js
@@ -1,10 +1,26 @@
 function HelloWorldPlugins(options) {
 
 }
-
 HelloWorldPlugins.prototype.apply = function (compiler) {
-    compiler.plugin('done', function () {
-        console.log('Hello World! Webpack!');
+
+    //编译(compilation)完成。
+    compiler.plugin('done', function (compilation) {
+        console.log('\nHello World! Webpack!' + ' done!');
+    });
+    // 生成资源到 output 目录之前。
+    compiler.plugin("emit", function (compilation, callback) {
+        console.log('\nHello World! Webpack!' + ' emit doing!');
+
+        callback();//emit事件是异步的，必须通过调callback继续下面的编辑。此处如果不调用，上面的done就不会触发。
+    });
+    // 编译(compilation)创建之后，执行插件。
+    compiler.plugin("compilation", function (compilation) {
+
+        // 优化阶段开始时触发。
+        compilation.plugin("optimize", function () {
+            console.log("Assets are being optimized.");
+        });
+
     });
 }
 
