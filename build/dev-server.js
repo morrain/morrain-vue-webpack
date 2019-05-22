aeed60c7b8d27c336f49d08c8699ccdc0abf273b
morarin <626100676@qq.com>
2019-05-22 16:06:09 +0800

    add check port func

--- build/dev-server.js
+++ build/dev-server.js
@@ -4,6 +4,8 @@ process.env.NODE_ENV = 'development';
 
 const webpackDevServer = require('webpack-dev-server');
 const webpack = require('webpack');
+const portfinder = require('portfinder');
+const chalk = require('chalk');
 
 const webpackConfig = require('./webpack.dev.config');
 const compiler = webpack(webpackConfig);
@@ -15,9 +17,25 @@ const webpackDevServerConfig = Object.assign({
 
 const server = new webpackDevServer(compiler, webpackDevServerConfig);
 
-server.listen(webpackDevServerConfig.port, webpackDevServerConfig.host, function () {
-    console.log('dev server listening on port:' + webpackDevServerConfig.port);
-    console.log();
-    console.log('http://' + webpackDevServerConfig.host + ':' + webpackDevServerConfig.port);
-    console.log();
+
+portfinder.basePort = webpackDevServerConfig.port;
+portfinder.getPortPromise().then(port => {
+
+    webpackConfig.devServer.port = port;
+    webpackDevServerConfig.port = port;
+
+    server.listen(port, webpackDevServerConfig.host, function () {
+
+        console.log(chalk.green('dev server listening on port:' + port));
+        console.log();
+        console.log(chalk.green('http://' + webpackDevServerConfig.host + ':' + port));
+        console.log();
+
+    });
+
+}).catch(error => {
+    console.log(chalk.red('can not find availabe port!'));
 });
+
+
+
