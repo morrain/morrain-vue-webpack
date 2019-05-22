b70e342c014af21fae1aa9f53ec8c929f1417e02
morarin <626100676@qq.com>
2019-05-22 17:02:03 +0800

    add   friendly-errors-webpack-plugin

--- build/dev-server.js
+++ build/dev-server.js
@@ -6,6 +6,7 @@ const webpackDevServer = require('webpack-dev-server');
 const webpack = require('webpack');
 const portfinder = require('portfinder');
 const chalk = require('chalk');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
 
 const webpackConfig = require('./webpack.dev.config');
 const webpackDevServerConfig = Object.assign({
@@ -19,20 +20,30 @@ portfinder.getPortPromise().then(port => {
     webpackConfig.devServer.port = port;
     webpackDevServerConfig.port = port;
 
+    webpackConfig.plugins.push(new FriendlyErrorsPlugin({
+        compilationSuccessInfo: {
+            messages: [
+                'You application is running here:\n',
+                chalk.green(`http://${webpackDevServerConfig.host}:${port}`)
+            ],
+            notes: ['You can click the link upside to get your site!']
+        },
+        onErrors: function (severity, errors) {
+            // You can listen to errors transformed and prioritized by the plugin
+            // severity can be 'error' or 'warning'
+        }
+    }));
+
     const compiler = webpack(webpackConfig);
     const server = new webpackDevServer(compiler, webpackDevServerConfig);
 
     server.listen(port, webpackDevServerConfig.host, function () {
-
-        console.log(chalk.green('dev server listening on port:' + port));
-        console.log();
-        console.log(chalk.green('http://' + webpackDevServerConfig.host + ':' + port));
-        console.log();
-
+        console.log(chalk.green('dev server is running on port:' + port));
     });
 
 }).catch(error => {
     console.log(chalk.red('can not find availabe port!'));
+    console.log(error);
 });
 
 
