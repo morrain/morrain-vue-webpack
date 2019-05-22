f79d4f31dfa79b452809bfab37d7009cd91271ea
morarin <626100676@qq.com>
2019-05-22 16:44:22 +0800

    fixbug: portfinder与webpack的异步没有处理好，导致hot功能失效

--- build/dev-server.js
+++ build/dev-server.js
@@ -8,21 +8,19 @@ const portfinder = require('portfinder');
 const chalk = require('chalk');
 
 const webpackConfig = require('./webpack.dev.config');
-const compiler = webpack(webpackConfig);
 const webpackDevServerConfig = Object.assign({
     stats: {
         colors: true
     }
 }, webpackConfig.devServer);
-
-const server = new webpackDevServer(compiler, webpackDevServerConfig);
-
-
 portfinder.basePort = webpackDevServerConfig.port;
 portfinder.getPortPromise().then(port => {
 
     webpackConfig.devServer.port = port;
     webpackDevServerConfig.port = port;
+
+    const compiler = webpack(webpackConfig);
+    const server = new webpackDevServer(compiler, webpackDevServerConfig);
 
     server.listen(port, webpackDevServerConfig.host, function () {
 
@@ -36,6 +34,11 @@ portfinder.getPortPromise().then(port => {
 }).catch(error => {
     console.log(chalk.red('can not find availabe port!'));
 });
+
+
+
+
+
 
 
 
